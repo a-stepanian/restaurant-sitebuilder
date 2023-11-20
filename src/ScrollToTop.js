2c55bcd5debc08dbccd29958c1ad99439a70a1b5
Alex Stepanian <alexstepanian11@gmail.com>
2023-11-20 07:38:08 -0800

    Hotfix scroll to top function

--- src/ScrollToTop.js
+++ src/ScrollToTop.js
@@ -5,12 +5,7 @@ export default function ScrollToTop({ children }) {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    const canControlScrollRestoration = "scrollRestoration" in window.history;
-    if (canControlScrollRestoration) {
-      window.history.scrollRestoration = "manual";
-    }
-
-    window.scrollTo(0, 0);
+    document.querySelector(".site-container").scrollTo(0, 0);
   }, [pathname]);
 
   return children;
